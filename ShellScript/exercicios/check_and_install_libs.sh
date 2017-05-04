02f83ea5077723041350bf12e5eb20e83f83b3a1
josinaldo <josinaldo.web@gmail.com>
2017-05-04 16:51:33 -0300

    update

---
+++ ShellScript/exercicios/check_and_install_libs.sh
@@ -0,0 +1,5 @@
+###################### Check libs #######################
+which pip > /dev/null || sudo easy_install pip
+which a2po > /dev/null || sudo easy_install android2po || sudo pip install termcolor
+#... others
+#########################################################
